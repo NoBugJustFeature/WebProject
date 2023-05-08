09bd83e9fb7df4260aa6f0c0cbf3b7e1b6bde551
NoBugJistFeature <maks_vinokurov_2005@mail.ru>
2023-05-08 18:19:06 +0300

    added comments and update registration

--- web/main/forms.py
+++ web/main/forms.py
@@ -1,14 +1,23 @@
 from django import forms
+from .models import Comments
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Логин')
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
 
-class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
-
+class RegistrationForm(UserCreationForm):
     class Meta:
-        model = User
-        fields = ('username', 'first_name', 'email')
+        model=User
+        fields = ['username','email']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model= Comments
+        fields = ["comment"]
+        widgets = {"comment": forms.Textarea(attrs={
+            "placeholder": "Оставьте свой отзыв",
+            'class': "textarea-style",
+            })
+        }
--- web/main/views.py
+++ web/main/views.py
@@ -5,20 +5,39 @@ from .models import Movies, Comments
 from django.contrib.auth.models import User
 
 from django.contrib.auth import authenticate, login, logout
-from .forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm, CommentForm
 
 
 def index(request):
     movies = Movies.objects.all()
     return render(request, "main/index.html", {"movies": movies})
 
+def registration(request):
+    if request.user.is_authenticated:
+        return redirect(account)
+    
+    message = None
+    user_form = RegistrationForm()
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(account)
+        message = form.errors
+
+        
+    return render(request, 'main/registration.html', {'user_form': user_form, 'message': message})
+
+
 def log_in(request):
     message = None
+    login_form = LoginForm()
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        login_form = LoginForm(request.POST)
 
-        if form.is_valid():
-            cd = form.cleaned_data
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
             user = authenticate(username=cd['username'], password=cd['password'])
 
             if user and user.is_active:
@@ -27,9 +46,7 @@ def log_in(request):
                 return redirect(account)
             
         message = "Неправильный логин или пароль"
-            
-    form = LoginForm()
-    return render(request, 'main/login.html', {'form': form, "message": message})
+    return render(request, 'main/login.html', {'form': login_form, "message": message})
 
 def account(request):
     if request.method == 'POST':
@@ -42,28 +59,24 @@ def account(request):
         return render(request, "main/account.html", {"user":user, "comments": comments})
     else: 
         return redirect(log_in)
-    
-def registration(request):
-    if request.user.is_authenticated:
-        return redirect(account)
-
-    if request.method == 'POST':
-        user_form = RegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
-            new_user.is_active = False
-            new_user.save()
-            return render(request, 'main/registration_done.html', {'new_user': new_user})
-    else:
-        user_form = RegistrationForm()
-        return render(request, 'main/registration.html', {'user_form': user_form})
 
 def about(request):
     return render(request, "main/about.html")
 
 def about_film(request):
+    if request.method == 'POST':
+        comment = CommentForm(request.POST)
+        if comment.is_valid():
+            comm = comment.save(commit=False)
+            comm.film_title = Movies.objects.get(href=request.GET.get("movie")).title
+            comm.username = request.user.username
+            comm.film_href = request.GET.get("movie")
+            comm.save()
+            
+
     movie = Movies.objects.get(href=request.GET.get("movie"))
     comments = Comments.objects.filter(film_title=movie.title)
+    leave_comment = CommentForm() 
     return render(request, "main/about-film.html", {"movie": movie,
-                                                    "comments": comments})
+                                                    "comments": comments,
+                                                    "leave_comment": leave_comment})
